311536b331d20ffafeb71ed85a513b5ea536158a
trilltrev <trevor1.taylor@famu.edu>
2020-10-13 20:22:43 -0400

    slatt

--- app/src/main/java/com/example/instagram1/ParseApplication.java
+++ app/src/main/java/com/example/instagram1/ParseApplication.java
@@ -3,11 +3,14 @@ package com.example.instagram1;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //register parse models
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("4F8yOFfqIu8JDCeNLYi5MUWqEBghdgdzGa7E0Ptu")
                 .clientKey("rwQfzqIDM0R6QNxWuOz4HyDcttdkiCOUvRBEvtk9")
